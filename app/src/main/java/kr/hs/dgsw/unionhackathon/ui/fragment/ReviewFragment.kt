c606d7093d81327b9598488d7ed45f5dd8126e88
tmddusqls <tmddusqls@naver.com>
2021-11-24 21:23:37 +0900

    review

---
+++ app/src/main/java/kr/hs/dgsw/unionhackathon/ui/fragment/ReviewFragment.kt
@@ -0,0 +1,22 @@
+package kr.hs.dgsw.unionhackathon.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.hs.dgsw.unionhackathon.R
+import kr.hs.dgsw.unionhackathon.databinding.FragmentReviewBinding
+
+class ReviewFragment : Fragment() {
+
+    private lateinit var binding: FragmentReviewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_review, container, false)
+    }
+
+}
